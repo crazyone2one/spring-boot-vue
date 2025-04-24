af1f031c6571f359eb6ae38626ef258719cb3f75
lijinglin <the2ndindec@gmail.com>
2025-04-24 11:36:19 +0800

    first commit

---
+++ backend/src/test/java/cn/master/phoenix/SpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.master.phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
