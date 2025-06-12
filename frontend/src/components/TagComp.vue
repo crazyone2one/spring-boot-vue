4e92266cf8d334ef75dd703a508895f2974c4f3b
lijinglin <the2ndindec@gmail.com>
2025-06-12 16:13:09 +0800

    feat(front): tag组件

---
+++ frontend/src/components/TagComp.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {NTag} from 'naive-ui'
+import type {IRole} from "/@/types/role.ts";
+
+withDefaults(defineProps<{
+  roles: Array<IRole>,
+  size?: 'small' | 'medium' | 'large'
+}>(), {
+  size: 'small'
+})
+</script>
+
+<template>
+  <n-flex>
+    <n-tag v-for="role in roles" :key="role.id" :bordered="false" type="info"
+           :size="size">
+      {{ role.name }}
+    </n-tag>
+  </n-flex>
+</template>
+
+<style scoped>
+
+</style>
