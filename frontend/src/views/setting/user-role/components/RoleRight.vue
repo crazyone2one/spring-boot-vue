42a9b0b09f90375da85c7347a307105e64022b3d
lijinglin <the2ndindec@gmail.com>
2025-06-13 17:35:14 +0800

    ✨ feat(role): 添加role功能

--- frontend/src/views/setting/user-role/components/RoleRight.vue
+++ frontend/src/views/setting/user-role/components/RoleRight.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import type {IRole} from "/@/types/role.ts";
 
+const currentRole = defineModel<Partial<IRole>>('role', {default: {}})
 </script>
 
 <template>
-  $END$
+  <div>
+    show something about selected role:
+    <p>
+      role name : {{currentRole.name}}
+    </p>
+    <p>
+      internal : {{currentRole.internal}}
+    </p>
+  </div>
 </template>
 
 <style scoped>
